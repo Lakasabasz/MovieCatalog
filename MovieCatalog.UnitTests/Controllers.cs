907456b3f3887dd8b2f02ac66f6e0e65efc4b007
Lakasabasz <lakasabasz@gmail.com>
2023-05-17 22:53:52 +0200

    Added unit tests

--- MovieCatalog.UnitTests/Controllers.cs
+++ MovieCatalog.UnitTests/Controllers.cs
@@ -1,15 +1,56 @@
+using Moq;
+using MovieCatalog.Controllers;
+using MovieCatalog.DTOs;
+using MovieCatalog.Models;
+using MovieCatalog.Services;
+
 namespace MovieCatalog.UnitTests;
 
-public class Tests
+[TestFixture]
+public class CatalogControllerTest
 {
+    private CatalogController _controller = null!;
+    
     [SetUp]
     public void Setup()
     {
+        var mock = new Mock<IMovieService>();
+        mock.Setup(x => x.GetLastMovie()).ReturnsAsync(new Movie());
+        mock.Setup(x => x.GetMoviesByGenre("test")).Returns(new[] { new Movie(), new Movie() });
+        mock.Setup(x => x.GetMoviesByYear(2007)).Returns(new[] { new Movie(), new Movie(), new Movie() });
+        _controller = new CatalogController(mock.Object);
     }
 
     [Test]
-    public void Test1()
+    public async Task TestGetLast()
     {
+        var movie = await _controller.GetLast();
+        Assert.IsNotNull(movie);
+    }
+
+    [Test]
+    public void TestGetByGenres()
+    {
+        var movies = _controller.GetByGenre("test");
+        Assert.That(movies.Count() == 2);
+    }
+
+    [Test]
+    public void TestGetByYear()
+    {
+        var movies = _controller.GetByYear(2007);
+        Assert.That(movies.Count() == 3);
+    }
+
+    [Test]
+    public void TestAddMovie()
+    {
+        _controller.AddMovie(new AddMovieRequest()
+        {
+            Genre = "",
+            Name = "",
+            Year = 2007
+        });
         Assert.Pass();
     }
 }
---
+++ MovieCatalog.UnitTests/MovieService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCatalog.DTOs;
+using MovieCatalog.Models;
+using MovieCatalog.Services;
+using NUnit.Framework;
+
+namespace MovieCatalog.UnitTests;
+
+[TestFixture, SingleThreaded]
+public class MovieService
+{
+    private Services.MovieService _service = null!;
+    private AppDbContext _db = null!;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase("mock");
+        _db = new AppDbContext(optionsBuilder.Options);
+        _service = new Services.MovieService(_db);
+    }
+
+    [Test, Order(1)]
+    public async Task TestAddObjects()
+    {
+        await _service.AddMovie(new AddMovieRequest()
+        {
+            Name = "Test1", Genre = "Test", Year = 2001, Description = "ABC"
+        });
+        await _service.AddMovie(new AddMovieRequest()
+        {
+            Name = "Test2", Genre = "Test", Year = 2001
+        });
+        await _service.AddMovie(new AddMovieRequest()
+        {
+            Name = "Test3", Genre = "Test", Year = 2002
+        });
+        await _service.AddMovie(new AddMovieRequest()
+        {
+            Name = "TestA", Genre = "Unit", Year = 2000, Description = "ABC"
+        });
+        await _service.AddMovie(new AddMovieRequest()
+        {
+            Name = "TestB", Genre = "Testing", Year = 2000
+        });
+        
+        Assert.That(_db.Movies.Count(), Is.EqualTo(5));
+    }
+    
+    [Test, Order(2)]
+    public async Task TestGetLast()
+    {
+        var movie = await _service.GetLastMovie();
+        Assert.That(movie?.Name, Is.EqualTo("TestB"));
+    }
+    
+    [Test, Order(2)]
+    public void TestGetByGenre()
+    {
+        var movies = _service.GetMoviesByGenre("Test").ToArray();
+        Assert.That(movies.Select(x => x.Genre), Is.All.EqualTo("Test"));
+        Assert.That(movies.Count(), Is.EqualTo(3));
+    }
+    
+    [Test, Order(2)]
+    public void TestGetByYear()
+    {
+        var movies = _service.GetMoviesByYear(2000).ToArray();
+        Assert.That(movies.Select(x => x.Year), Is.All.EqualTo(2000));
+        Assert.That(movies.Count(), Is.EqualTo(2));
+    }
+    
+}
